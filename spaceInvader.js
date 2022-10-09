e01fb915461c99bc4517ed43134110ccaab52413
Marten <marten.driesen@student.ehb.be>
2022-10-09 19:44:58 +0200

    Made Space Invader assignment

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,54 @@
 "use strict";
 
-alert("Draw your space invader here");
+drawRectangles();
+
+function drawRectangles() {
+
+    let canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let context = canvas.getContext("2d");
+
+    context.lineWidth = 0;
+
+    //links boven
+
+    context.fillStyle = 'black';
+    context.beginPath();
+    context.rect(50, 50, 300, 300);
+    context.fill();
+    context.stroke();
+
+    context.fillStyle = 'green';
+    context.beginPath();
+    context.rect(125, 125, 50, 50);
+    context.fill();
+    context.stroke();
+
+
+    context.beginPath();
+    context.rect(225, 125, 50, 50);
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.rect(175, 175, 50, 50);
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.rect(125, 225, 50, 50);
+    context.fill();
+    context.stroke();
+
+    context.beginPath();
+    context.rect(225, 225, 50, 50);
+    context.fill();
+    context.stroke();
+
+
+
+
+
+
+}
